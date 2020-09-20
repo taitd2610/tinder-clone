80e4ef344b632b6da52749dd7caf2a240bdc6d22
taitd2610 <taitd153@gmail.com>
2020-09-21 00:47:05 +0700

    commit

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "./Header";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app">
+      <Header />
+      {/* TinderCards */}
+      {/* SwipeButton */}
+    </div>
+  );
+}
+
+export default App;
